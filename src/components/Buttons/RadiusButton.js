e032586c93fa128ef3469ed8da99e195bc6dac9b
Cullen6732 <Cullen6732@gmail.com>
2022-01-11 20:13:17 +0900

    [28기 엄성용] Add: RadiusButton 생성 중

--- src/components/Buttons/RadiusButton.js
+++ src/components/Buttons/RadiusButton.js
@@ -1,3 +1,99 @@
-export default function RadiusButton() {
-  return <>RadiusButton</>;
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faFacebookF,
+  faTwitter,
+  faPinterestP,
+  faLinkedinIn,
+} from '@fortawesome/free-brands-svg-icons';
+import {
+  faHeart,
+  faShare,
+  faShareAlt,
+  faChevronLeft,
+  faChevronRight,
+} from '@fortawesome/free-solid-svg-icons';
+import styled from 'styled-components';
+
+const BUTTONTYPE_OBJ = {
+  visitsite: 'fillColor',
+  collect: 'hoverEffect',
+};
+
+const buttonBodyContent = buttonType => {
+  switch (buttonType) {
+    case 'faceBookIcon':
+      return <FontAwesomeIcon icon={faFacebookF} />;
+    case 'heartIcon':
+      return <FontAwesomeIcon icon={faHeart} />;
+    case 'twitterIcon':
+      return <FontAwesomeIcon icon={faTwitter} />;
+    case 'painterestIcon':
+      return <FontAwesomeIcon icon={faPinterestP} />;
+    case 'linkedinIcon':
+      return <FontAwesomeIcon icon={faLinkedinIn} />;
+    case 'shareIcon':
+      return <FontAwesomeIcon icon={faShareAlt} />;
+    case 'siteIcon':
+      return <FontAwesomeIcon icon={faShare} />;
+    case 'leftIcon':
+      return <FontAwesomeIcon icon={faChevronLeft} />;
+    case 'rightIcon':
+      return <FontAwesomeIcon icon={faChevronRight} />;
+    default:
+      return buttonType;
+  }
+};
+
+export default function RadiusButton({ buttonType }) {
+  return (
+    <Components propColor={buttonType} className={BUTTONTYPE_OBJ[buttonType]}>
+      {buttonBodyContent(buttonType)}
+    </Components>
+  );
 }
+
+const Components = styled.button`
+  color: ${props => {
+    return props.propColor.includes('Icon') ? 'white' : 'black';
+  }};
+  display: flex;
+  align-items: center;
+  padding: 0 10px;
+  height: 20px;
+  border-radius: 34px;
+  border-color: #202121;
+  background-color: transparent;
+
+  &:hover {
+    color: #49c5b6;
+    border-color: #49c5b6;
+    transition: all 0.3s ease;
+  }
+
+  &.fillColor {
+    color: white;
+    border-color: #49c5b6;
+    background-color: #49c5b6;
+    &:hover {
+      opacity: 0.8;
+      transition: all 0.3s ease;
+    }
+  }
+
+  &.hoverEffect {
+    transition: all 0.3s ease;
+    background-color: transparent;
+    border-color: #fcfcfc;
+    color: #fcfcfc;
+    &:hover {
+      color: #49c5b6;
+      border-color: #49c5b6;
+    }
+  }
+
+  &.white {
+    border-color: #fcfcfc;
+    color: #fcfcfc;
+  }
+`;
