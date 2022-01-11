94116093cd94aacd7da377e42addeaf2cfe2b7eb
KimJeongHyun <ptr1129@hanmail.net>
2022-01-11 19:14:38 +0900

    [Add] PaginationButtons 구현
    [Modify] 페이지네이션 버튼 스타일 수정

---
+++ src/customlib/classNameToggle.js
@@ -0,0 +1,2 @@
+export const cn = (...args) => args.join(' ');
+export const cond = (cond, className) => (cond ? className : '');
--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -1,8 +1,6 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
-@import url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR&display=swap');
-
 body {
   width: 100vw;
   height: 100vh;
