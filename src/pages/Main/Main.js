c4e669a38c6079e0ec07df16c5d8f73345a8cef5
Cullen6732 <Cullen6732@gmail.com>
2022-01-12 16:29:14 +0900

    [28기 엄성용][Add] Nav bar 구현 시작

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,5 +1,15 @@
 import React from 'react';
+import Nav from '../../components/Nav/Nav';
+import './Main.scss';
 
 export default function Main() {
-  return <>Main!</>;
+  return (
+    <div>
+      <Nav />
+      <img
+        src="https://search.pstatic.net/sunny/?src=https%3A%2F%2Fimage.utoimage.com%2Fpreview%2Fcp864374%2F2016%2F03%2F201603043228_500.jpg&type=a340"
+        height="1200px"
+      />
+    </div>
+  );
 }
