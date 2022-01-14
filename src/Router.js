182353d1344a78b82d6768842081305688d0f635
SeongYong <cullen@SeongYongui-MacBookPro.local>
2022-01-14 17:58:10 +0900

    [28기 엄성용] Router 수정- Nav & footer 추가

--- src/Router.js
+++ src/Router.js
@@ -1,19 +1,22 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
+import Nav from './components/Nav/Nav';
 import Main from './pages/Main/Main';
 import TripList from './pages/TripList/TripList';
 import TripDetail from './pages/TripDetail/TripDetail';
 import NotFound from './components/NotFound/NotFound';
+import Footer from './components/Footer/Footer';
 
 export default function Router() {
   return (
     <BrowserRouter>
+      <Nav />
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/TripList" element={<TripList />} />
         <Route path="/TripDetail" element={<TripDetail />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 }
